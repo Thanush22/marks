a81bd761a7f68389dc0f8eeabaabc6d5021c16d2
Thanush22 <95023151+Thanush22@users.noreply.github.com>
2022-09-21 21:56:06 +0530

    Add files via upload

---
+++ marks.java
@@ -0,0 +1,36 @@
+package Collegeinformation;
+
+public class marks {
+    public static void main(String[]args){
+        marks subject = new marks();
+        subject.OODP();
+        subject.COA();
+        subject.DSA();
+        subject.ADE();
+        subject.BMPE();
+    }
+    public void OODP() {
+        int OODP = 17;
+        System.out.println(OODP);
+    }
+    public void COA(){
+        int COA = 18;
+        System.out.println(COA);
+    }
+    public void DSA(){
+        int DSA = 17;
+        System.out.println(DSA);
+    }
+    public void ADE(){
+        int ADE = 4;
+        System.out.println(ADE);
+    }
+    public void BMPE(){
+        int BMPE = 24;
+        System.out.println(BMPE);
+    }
+    public void MATHS(){
+        int MATHS = 15;
+        System.out.println(MATHS);
+    }
+}
